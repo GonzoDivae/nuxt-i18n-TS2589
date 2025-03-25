474f643511f8ffb63ba8953f7a0b42b6c658593a
GonzoDivae <david.gonzalezcasin@diva-e.com>
2025-03-25 17:05:36 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const { localeProperties } = useI18n();
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt I18n Starter - {{ $t('hello') }}!</h1>
+    {{ localeProperties }}
+  </div>
+</template>
